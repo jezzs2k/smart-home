da863903b8afdbe016c26f6aa8b0a2f28f7b9eb5
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-09-28 15:48:57 +0700

    add icon and splash screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import BottomTabs from './src/navigations';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View style={styles.container}>
       <BottomTabs />
